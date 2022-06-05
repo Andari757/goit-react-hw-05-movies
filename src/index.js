5b84bf145573545d752a3bb7e990159741bfab02
Andari757 <matviyskrypka@gmail.com>
2022-06-05 19:08:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/https://github.com/Andari757/goit-react-hw-05-movies//">
       <App />
     </BrowserRouter>
   </React.StrictMode>
